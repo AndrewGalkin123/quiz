269cd8eaa799f2faf5f2e33a2378686bec9a9785
AndrewGalkin123 <127788127788a@gmail.com>
2023-10-30 22:18:27 +0200

    quiz site

---
+++ src/components/UI/Cover/Cover.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "../../../styles/App.css"
+import EnterButton from "../enterButton/EnterButton"
+
+const Cover = () => {
+    return (
+        <div className="cover">
+            <p className="cover-title">QUIZ</p>
+            <EnterButton></EnterButton>
+
+
+
+
+        </div>
+    )
+}
+
+export default Cover
---
+++ src/components/UI/enterButton/EnterButton.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "../../../styles/App.css"
+
+const EnterButton = () => {
+    return (
+        <a href="/quiz">
+            <button className="enter-button">
+                Enter
+            </button>
+        </a>
+    )
+}
+
+export default EnterButton
